9968ceb17e950a337f68ce693caf8a9e09a867c3
maspi83 <69327668+maspi83@users.noreply.github.com>
2021-03-29 22:07:46 +0200

    Create .mod-bash-mac.sh

---
+++ src_files/.mod-bash-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/.kube_ps1.sh
+source ~/.git-completion.bash
+source ~/.git-prompt.sh
+
+DEFAULT="\[\033[0;00m\]"
+GREEN="\[\033[0;32m\]"
+YELLOW="\[\033[0;33m\]"
+CYAN="\[\033[0;36m\]"
+
+export PS1=$GREEN"\u@\h"$YELLOW" \w"$CYAN'$(__git_ps1 " (%s)") $(kube_ps1)'$DEFAULT"\n\$ "
